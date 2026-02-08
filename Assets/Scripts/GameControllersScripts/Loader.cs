23b17e3fa913e560db74fd6cad6488c5c9ae59c2
ZonBonn <iamzonbon@gmail.com>
2026-02-08 20:45:54 +0700

    change data date 8/2/26 20:45' keep doing progress loading scene

--- Assets/Scripts/GameControllersScripts/Loader.cs
+++ Assets/Scripts/GameControllersScripts/Loader.cs
@@ -13,15 +13,19 @@ public static class Loader
         LoadingScene,
     }
 
-    public static Action OnSceneCallback;
+    public static Action OnSceneCallback; // gọi load màn tiếp theo
 
     public static void Load(Scene scene)
     {
+        // đăng ký hàm gọi scene tiếp theo để khi nào tới loading scene thì loading scene sẽ gọi cái màn mà cần tới
         OnSceneCallback = () => {
-            SceneManager.LoadScene(scene.ToString());
+            FunctionTimer.Create(() => { SceneManager.LoadSceneAsync(scene.ToString()); }, 1f);
+            
         };
 
-        SceneManager.LoadScene(Scene.LoadingScene.ToString());
+        // OnSceneCallback = () => { SceneManager.LoadScene(scene.ToString()); }; 
+
+        SceneManager.LoadSceneAsync(Scene.LoadingScene.ToString());
     }
 
     public static void sceneCallback()
